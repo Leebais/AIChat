61a2b7f3e466568026464bef57306224bc25ea88
Leebais <cd1512240495@Outlook.com>
2020-09-10 09:58:50 +0800

    creat project
    creat project

---
+++ cmake-build-debug/CMakeFiles/AIChat.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AIChat"
+  "AIChat.pdb"
+  "CMakeFiles/AIChat.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AIChat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
